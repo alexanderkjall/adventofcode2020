0cc158cd7d168bc7b78531b1beed2529766af5f0
Alexander Kjäll <alexander.kjall@gmail.com>
2020-12-01 17:39:30 +0100

    fixed clippy complaints

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,17 @@
 use anyhow::anyhow;
 
 pub fn run() -> Result<(), anyhow::Error> {
-    let input:String = std::fs::read_to_string("res/day1-input")?.parse()?;
+    let input: String = std::fs::read_to_string("res/day1-input")?.parse()?;
 
-    let input_vec:Vec<i32> = input.split("\n").map(|s| s.parse::<i32>().unwrap_or(0)).filter(|i| i > &0).collect();
-    let result_1 = calculate_part_1(input_vec.clone())?;
-    let result_2 = calculate_part_2(input_vec)?;
+    let mut input_vec: Vec<i32> = input
+        .split('\n')
+        .map(|s| s.parse::<i32>().unwrap_or(0))
+        .filter(|i| i > &0)
+        .collect();
+    input_vec.sort_unstable();
+
+    let result_1 = calculate_part_1(&input_vec)?;
+    let result_2 = calculate_part_2(&input_vec)?;
 
     println!("result day 1 part 1 {}", result_1);
     println!("result day 1 part 2 {}", result_2);
@@ -13,18 +19,15 @@ pub fn run() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-fn calculate_part_1(mut input: Vec<i32>) -> Result<i32, anyhow::Error> {
-    input.sort();
-
-    for i in &input {
-        for j in &input {
+fn calculate_part_1(input: &[i32]) -> Result<i32, anyhow::Error> {
+    for i in input {
+        for j in input {
             let add = i + j;
 
-            if add > 2020 {
-                break;
-            }
-            else if add == 2020 {
-                return Ok(i * j);
+            match add {
+                2021..=std::i32::MAX => break,
+                2020 => return Ok(i * j),
+                _ => {}
             }
         }
     }
@@ -32,19 +35,16 @@ fn calculate_part_1(mut input: Vec<i32>) -> Result<i32, anyhow::Error> {
     Err(anyhow!("no two numbers add to 2020"))
 }
 
-fn calculate_part_2(mut input: Vec<i32>) -> Result<i32, anyhow::Error> {
-    input.sort();
-
-    for i in &input {
-        for j in &input {
-            for k in &input {
+fn calculate_part_2(input: &[i32]) -> Result<i32, anyhow::Error> {
+    for i in input {
+        for j in input {
+            for k in input {
                 let add = i + j + k;
 
-                if add > 2020 {
-                    break;
-                }
-                else if add == 2020 {
-                    return Ok(i * j * k);
+                match add {
+                    2021..=std::i32::MAX => break,
+                    2020 => return Ok(i * j * k),
+                    _ => {}
                 }
             }
         }
@@ -55,10 +55,14 @@ fn calculate_part_2(mut input: Vec<i32>) -> Result<i32, anyhow::Error> {
 
 #[test]
 fn test_calculate_part_1() {
-    assert_eq!(514579, calculate_part_1(vec![1721, 979, 366, 299, 675, 1456]).unwrap_or(0));
+    let mut input: Vec<i32> = vec![1721, 979, 366, 299, 675, 1456];
+    input.sort_unstable();
+    assert_eq!(514579, calculate_part_1(&input).unwrap_or(0));
 }
 
 #[test]
 fn test_calculate_part_2() {
-    assert_eq!(241861950, calculate_part_2(vec![1721, 979, 366, 299, 675, 1456]).unwrap_or(0));
+    let mut input: Vec<i32> = vec![1721, 979, 366, 299, 675, 1456];
+    input.sort_unstable();
+    assert_eq!(241861950, calculate_part_2(&input).unwrap_or(0));
 }
