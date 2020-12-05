4f758e3d1748c5d20f9746fb0dc2e5f73bf84112
Alexander Kjäll <alexander.kjall@gmail.com>
2020-12-05 11:51:42 +0100

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -4,7 +4,7 @@ use nom::bytes::complete::{tag, take_while};
 use nom::combinator::map_res;
 use nom::lib::std::collections::HashMap;
 use nom::sequence::tuple;
-use nom::IResult;
+use nom::{AsChar, IResult};
 
 struct Passport {
     ecl: Option<String>,
@@ -28,6 +28,109 @@ impl Passport {
             && self.iyr.is_some()
             && self.hgt.is_some()
     }
+
+    fn validate_all_fields(&self) -> bool {
+        validate_ecl(&self.ecl)
+            && validate_pid(&self.pid)
+            && validate_eyr(&self.eyr)
+            && validate_hcl(&self.hcl)
+            && validate_byr(&self.byr)
+            && validate_iyr(&self.iyr)
+            && validate_hgt(&self.hgt)
+    }
+}
+
+fn validate_hgt(hgt: &Option<String>) -> bool {
+    match hgt {
+        Some(hgt) => {
+            if hgt.ends_with("in") {
+                let l = hgt[..hgt.len() - 2].parse::<i32>().unwrap();
+
+                return (59..=76).contains(&l);
+            }
+            if hgt.ends_with("cm") {
+                let l = hgt[..hgt.len() - 2].parse::<i32>().unwrap();
+
+                return (150..=193).contains(&l);
+            }
+            false
+        }
+        None => false,
+    }
+}
+
+fn validate_iyr(iyr: &Option<String>) -> bool {
+    match iyr {
+        Some(iyr) => {
+            if iyr.len() != 4 || iyr.chars().take_while(|c| c.is_numeric()).count() != 4 {
+                return false;
+            }
+
+            let year = iyr.parse::<i32>().unwrap();
+            (2010..=2020).contains(&year)
+        }
+        None => false,
+    }
+}
+
+fn validate_byr(byr: &Option<String>) -> bool {
+    match byr {
+        Some(byr) => {
+            if byr.len() != 4 || byr.chars().take_while(|c| c.is_numeric()).count() != 4 {
+                return false;
+            }
+
+            let year = byr.parse::<i32>().unwrap();
+            (1920..=2002).contains(&year)
+        }
+        None => false,
+    }
+}
+
+fn validate_hcl(hcl: &Option<String>) -> bool {
+    match hcl {
+        Some(hcl) => {
+            if !hcl.starts_with('#') {
+                return false;
+            }
+
+            hcl.chars().skip(1).take_while(|c| c.is_hex_digit()).count() == 6
+        }
+        None => false,
+    }
+}
+
+fn validate_eyr(eyr: &Option<String>) -> bool {
+    match eyr {
+        Some(eyr) => {
+            if eyr.len() != 4 || eyr.chars().take_while(|c| c.is_numeric()).count() != 4 {
+                return false;
+            }
+
+            let year = eyr.parse::<i32>().unwrap();
+            (2020..=2030).contains(&year)
+        }
+        None => false,
+    }
+}
+
+fn validate_pid(pid: &Option<String>) -> bool {
+    match pid {
+        Some(pid) => pid.len() == 9 && pid.chars().take_while(|c| c.is_numeric()).count() == 9,
+        None => false,
+    }
+}
+
+fn validate_ecl(ecl: &Option<String>) -> bool {
+    match ecl {
+        Some(ecl) => {
+            matches!(
+                &ecl[..],
+                "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth"
+            )
+        }
+        None => false,
+    }
 }
 
 pub fn run() -> Result<(), anyhow::Error> {
@@ -36,8 +139,10 @@ pub fn run() -> Result<(), anyhow::Error> {
     let passports = parse_passports(&input)?;
 
     let result_1 = passports.iter().filter(|p| p.is_valid()).count();
+    let result_2 = passports.iter().filter(|p| p.validate_all_fields()).count();
 
     println!("result day 4 part 1 {}", result_1);
+    println!("result day 4 part 2 {}", result_2);
 
     Ok(())
 }
@@ -139,4 +244,116 @@ iyr:2011 ecl:brn hgt:59in";
     let result_1 = passports.iter().filter(|p| p.is_valid()).count();
 
     assert_eq!(2, result_1);
+}
+
+#[test]
+fn byr_valid() {
+    assert_eq!(true, validate_byr(&Some("2002".to_string())))
+}
+
+#[test]
+fn byr_invalid() {
+    assert_eq!(false, validate_byr(&Some("2003".to_string())))
+}
+
+#[test]
+fn hgt_valid1() {
+    assert_eq!(true, validate_hgt(&Some("60in".to_string())))
+}
+
+#[test]
+fn hgt_valid2() {
+    assert_eq!(true, validate_hgt(&Some("190cm".to_string())))
+}
+
+#[test]
+fn hgt_invalid1() {
+    assert_eq!(false, validate_hgt(&Some("190in".to_string())))
+}
+
+#[test]
+fn hgt_invalid2() {
+    assert_eq!(false, validate_hgt(&Some("190".to_string())))
+}
+
+#[test]
+fn hcl_valid() {
+    assert_eq!(true, validate_hcl(&Some("#123abc".to_string())))
+}
+
+#[test]
+fn hcl_invalid1() {
+    assert_eq!(false, validate_byr(&Some("#123abz".to_string())))
+}
+
+#[test]
+fn hcl_invalid2() {
+    assert_eq!(false, validate_byr(&Some("123abc".to_string())))
+}
+
+#[test]
+fn ecl_valid() {
+    assert_eq!(true, validate_ecl(&Some("brn".to_string())))
+}
+
+#[test]
+fn ecl_invalid() {
+    assert_eq!(false, validate_ecl(&Some("wat".to_string())))
+}
+
+#[test]
+fn pid_valid() {
+    assert_eq!(true, validate_pid(&Some("000000001".to_string())))
+}
+
+#[test]
+fn pid_invalid() {
+    assert_eq!(false, validate_pid(&Some("0123456789".to_string())))
+}
+
+#[test]
+fn test_strict_validation_all_invalid() {
+    let input = "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+
+    let passports = parse_passports(&input).unwrap();
+
+    let result_1 = passports.iter().filter(|p| p.validate_all_fields()).count();
+
+    assert_eq!(4, passports.len());
+    assert_eq!(0, result_1);
+}
+
+#[test]
+fn test_strict_validation_all_valid() {
+    let input = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+
+    let passports = parse_passports(&input).unwrap();
+
+    let result_1 = passports.iter().filter(|p| p.validate_all_fields()).count();
+
+    assert_eq!(4, passports.len());
+    assert_eq!(4, result_1);
 }
